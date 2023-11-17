f582a99a254295ae1a5cea4574b8a7239ac54e57
yumi-0223 <chym22048@g.nihon-u.ac.jp>
2023-11-17 18:39:41 +0900

    完成!!!!

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,70 @@
+import React, { useState } from 'react';
+
 export default function App() {
-    const showStory = false;
-    const xItem = "";
-    const yItem = "";
-    const zItem = "";
-    const ukus = "us";
+  const [showStory,setShowStory] = useState(false);
+  const [xItem,setXItem] = useState("");
+  const [yItem,setYItem] = useState("");
+  const [zItem,setZItem] = useState("");
+  const [name,setName]=useState("Bob");
+  const [usuk,setUsuk] = useState("us");
 
-    //配列の中から、要素を決定する
-    function randomValueFromArray(array){
-        const random = Math.floor(Math.random()*array.length);
-        return array[random];
-    }
-
-    return (
-      <>
-        <div>
-          <label htmlFor="customname">Enter custom name:</label>
-          <input type="text" placeholder="" />
-        </div>
-        <div>
-          <label htmlFor="us">US</label>
-          <input type="radio" value="us" checked={ukus === "us"} />
-          <label htmlFor="uk">UK</label>
-          <input type="radio" value="uk" checked={ukus === "uk"} />
-        </div>
-        <div>
-          <button>Generate random story</button>
-        </div>
-
-
-　　
-        
-        {showStory && (
-          <p>
-            It was 94 fahrenheit outside, so {xItem} went for a walk. When they
-            got to {yItem}, they stared in horror for a few moments, then {zItem}.
-            Bob saw the whole thing, but was not surprised — {xItem} weighs 300
-            pounds, and it was a hot day.
-          </p>
-        )}
-      </>
-    );
+  function randomValueFromArray(array){
+    const random = Math.floor(Math.random()*array.length);
+    return array[random];
   }
+  
+
+
+
+
+  return (
+    <>
+    <form onSubmit={(event)=>{
+    event.preventDefault();
+    setShowStory(true)
+    setXItem(randomValueFromArray(["Willy the Goblin","Big Daddy","Father Christmas"]))
+    setYItem(randomValueFromArray(["the soup kitchen","Disneyland","the White House"]))
+    setZItem(randomValueFromArray(["spontaneously combusted","melted into a puddle on the sidewalk","turned into a slug and crawled away"]))
+    
+    const usukField=event.target.elements.usukField;
+    if(usukField.value==="us"){
+      setUsuk("us")
+    }else{
+      setUsuk("uk")
+    }
+    const customNameField=event.target.elements.customname;
+    if(customNameField.value){
+      setName(customNameField.value)
+    }else{
+      setName(Bob);
+    }
+  
+  }}
+
+    >
+
+      <div>
+        <label htmlFor="customname">Enter custom name:</label>
+        <input type="text" placeholder="" name="customname"/>
+      </div>
+      <div>
+        <label htmlFor="usukField">US</label>
+        <input type="radio" name="usukField" id="us" value="us" defaultChecked/>
+        <label htmlFor="usukField">UK</label>
+        <input type="radio" name="usukField" id="uk" value="uk" />
+      </div>
+      <div>
+      <button type="submit" >Generate random story</button>
+      </div>     
+      {showStory && (
+        <p>
+          It was {usuk === "us" ? "94" : "34"} fahrenheit outside, so {xItem} went for a walk. When they
+          got to {yItem}, they stared in horror for a few moments, then {zItem}.
+          {name} saw the whole thing, but was not surprised — {xItem} weighs {usuk === "us"? "300 pounds" : "21 stone"}
+          , and it was a hot day.
+        </p>
+      )}
+      </form>
+    </>
+  );
+}
